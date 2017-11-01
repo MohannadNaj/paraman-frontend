9894173ff7539e40fa6c5ef8c48cb9def78f3cf7
MohannadNaj <me@mohannad.io>
2017-11-01 03:31:42 +0300

    Remove unused Blade Views

--- resources/views/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('parameters::shared.parameters-layout')
-
-@section('content')
-  <parameters ref="all-parameters" :parameters_list='{!! $parameters->toJson(JSON_HEX_APOS) !!}'></parameters>
-@endsection
-

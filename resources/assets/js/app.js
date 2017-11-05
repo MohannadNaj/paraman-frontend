6fcda07ff2268a5832309edf9e7be8e486924620
MohannadNaj <me@mohannad.io>
2017-11-05 04:54:27 +0300

    Add new `paraman` component, Refactor to make it act as the root

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,19 +1,8 @@
 require('./core')
 require('./bootstrap')
 
-Vue.component('dropzone-upload', require('./components/dropzone-upload.vue'))
-Vue.component('modal', require('./components/modal.vue'))
-
-Vue.component('parameters', require('./components/parameters.vue'))
+Vue.component('paraman', require('./components/paraman.vue'))
 
 const app = new Vue({
-  el: '#app',
-  mounted() {
-    Helper.modal = this.$refs['modal']
-    Helper.dropzoneModal = this.$refs['dropzone-modal']
-
-    if (window.Laravel.needInstallation) {
-      EventBus.fire('need-installation')
-    }
-  }
+  el: '#app'
 })
---
+++ resources/assets/js/components/paraman.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <slot>
+            <parameters ref="all-parameters" :parameters_list='parametersList'></parameters>
+            <!-- Helpers -->
+            <notifications></notifications>
+            <modal ref="modal" id="modal"></modal>
+            <dropzone-upload ref="dropzone-modal" _target="parameters/addPhoto" _update_target="parameters/updatePhoto"></dropzone-upload>
+        </slot>
+    </div>
+</template>
+
+<script>
+import parameters from './parameters.vue'
+import modal from './modal.vue'
+import dropzoneUpload from './dropzone-upload.vue'
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        parametersList: {default: ()=> []}
+    },
+    components: {
+        'dropzone-upload': dropzoneUpload,
+        'parameters': parameters,
+        'modal': modal,
+    },
+    mounted() {
+        Helper.modal = this.$refs['modal']
+        Helper.dropzoneModal = this.$refs['dropzone-modal']
+
+        if (window.Laravel.needInstallation) {
+          EventBus.fire('need-installation')
+        }
+    }
+}
+</script>

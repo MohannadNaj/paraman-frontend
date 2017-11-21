b0a1c0c74c42007a15019eb536a8b5989ad22d94
MohannadNaj <me@mohannad.io>
2017-11-21 22:02:59 +0300

    add installer component test

--- specs/installer.spec.js
+++ specs/installer.spec.js
@@ -1,18 +1,75 @@
 import installer from '../src/js/components/installer'
+import _package from '../package.json'
 
-xdescribe('installer Component', () => {
-  var fakeModal;
-
-  var parameter = TestData.modified_parameters[0]
-
+describe('installer Component', () => {
   beforeEach(()=> {
     commonBeforeEach(installer)
+    window.Laravel.installationData = {
+            databasePath: 'databasePath',
+            migrationPaths: 'migrationPaths'
+        }
   })
 
   afterEach(commonAfterEach)
 
-  it('....', () => {
+  it('has the public properties used in the test suite', () => {
     createVue()
+    expect(typeof vm.getStepByAction)
+    .toBe('function')
+    expect(typeof vm._computedWatchers.getActiveStep)
+    .toBe('object')
+  })
+
+  it('shows package version', () => {
+    createVue()
+
+    expect(vm.$el.textContent)
+    .toContain(_package.version)
+  })
+
+  it(`has 'createDatabase' and 'migrate' steps`, () => {
+    createVue()
+
+    let actionExist = (action) => { 
+        expect(vm.steps.map(x => x.action))
+        .toContain(action)
+    }
+
+    ;['createDatabase','migrate'].forEach(actionExist)
+  })
+
+  it('shows only the active step', (done) => {
+    window.Laravel.needInstallation = false
+    window.Laravel.needMigration = true
+
+    createVue()
+
+    expect(vm.$el.textContent)
+    .not.toContain(vm.getStepByAction('createDatabase').text)
+
+    then(() => {
+        expect(vm.$el.textContent)
+        .toContain(vm.getStepByAction('migrate').text)
+        done()
+    })
+  })
+
+  it(`set the correct step state for 'createDatabase'`, () => {
+    window.Laravel.needInstallation = false
+
+    createVue()
+
+    expect(vm.getStepByAction('createDatabase').isDone)
+    .toBeTruthy()
+  })
+
+  it(`set the correct step state for 'migrate'`, () => {
+    window.Laravel.needMigration = false
+
+    createVue()
+
+    expect(vm.getStepByAction('migrate').isDone)
+    .toBeTruthy()
   })
 })
 

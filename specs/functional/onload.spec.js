a9a81f2c4040a7a7fe64503066562d3ae3939d84
MohannadNaj <me@mohannad.io>
2017-11-30 05:19:18 +0300

    add more onload tests

--- specs/functional/onload.spec.js
+++ specs/functional/onload.spec.js
@@ -1,7 +1,7 @@
 import paraman from '../../src/js/components/paraman'
 import _package from '../../package.json'
 
-describe('functional: onload', () => {
+fdescribe('functional: onload', () => {
   beforeEach(()=> {
     commonBeforeEach(paraman)
     window.Laravel.installationData = {
@@ -75,7 +75,7 @@ describe('functional: onload', () => {
     })
   })
 
-  it(`shows parameter data if parametersList is not empty`, (done) => {
+  it(`shows parameter data and open category -whatever it's- if parametersList is not empty`, (done) => {
     paramanVue([TestData.parameters[0]])
 
     next(() => {
@@ -84,6 +84,106 @@ describe('functional: onload', () => {
          expect(textContent())
         .toContain(TestData.parameters[0][prop])
       })
+      expectEvent('opening-category')
+      done()
+    }, 10)
+  })
+
+  it(`open the first category and shows it's parameters data`, (done) => {
+    let activeCategory = TestData.categories[0] // zero index = first category
+    let activeCategoryParameters = TestData.categorized_parameters.filter(x => x.category_id == activeCategory.id )
+
+    paramanVue([...TestData.categories , ...TestData.categorized_parameters])
+
+    next(() => {
+      // expected event
+      expectEvent('opening-category')
+
+      // first category's name
+      expect(vm.$el.querySelector('.parameters-list--header').textContent)
+      .toContain(activeCategory.value)
+
+      // first category's parameters length
+      expect(vm.$el.querySelector('.parameters-list--header').textContent)
+      .toContain(activeCategoryParameters.length)
+
+      // first category's parameters data
+      ;['label', 'humanizedUpdatedAt', 'type']
+      .forEach((prop) => {
+        // first parameter in the active category is present
+        expect(textContent())
+        .toContain(activeCategoryParameters[0][prop])
+      })
+
+      done()
+    }, 10)
+  })
+
+  it(`open the category in the location hash and shows it's parameters data`, (done) => {
+    let testedCategoryindex = parseInt(TestData.categories.length / 2)
+    // we can't assert the zero index because it's the default, so we will write
+    // an assertion for that
+    expect(testedCategoryindex)
+    .not.toBe(0)
+
+    let activeCategory = TestData.categories[testedCategoryindex]
+    window.location.hash = `#${activeCategory.id}`
+
+    let activeCategoryParameters = TestData.categorized_parameters.filter(x => x.category_id == activeCategory.id )
+
+    paramanVue([...TestData.categories , ...TestData.categorized_parameters])
+
+    next(() => {
+      // expected event
+      expectEvent('opening-category')
+
+      // location-hash category's name
+      expect(vm.$el.querySelector('.parameters-list--header').textContent)
+      .toContain(activeCategory.value)
+
+      // location-hash category's parameters length
+      expect(vm.$el.querySelector('.parameters-list--header').textContent)
+      .toContain(activeCategoryParameters.length)
+
+      // location-hash category's parameters data
+      ;['label', 'humanizedUpdatedAt', 'type']
+      .forEach((prop) => {
+        // first parameter in the active category is present
+        expect(textContent())
+        .toContain(activeCategoryParameters[0][prop])
+      })
+
+      done()
+    }, 10)
+  })
+
+  it(`if the location hash is 'categories' ignore it`, (done) => {
+    window.location.hash = `#categories`
+ 
+    let activeCategory = TestData.categories[1]
+
+    paramanVue([...TestData.categories , ...TestData.categorized_parameters])
+
+    next(() => {
+      // 
+      expect(vm.$el.querySelector('.parameters-list--header').textContent.toLowerCase())
+      .not.toContain('categories')
+
+      done()
+    }, 10)
+  })
+
+  it(`set the location hash to the default loaded category (first one)`, (done) => {
+
+    let activeCategory = TestData.categories[0]
+
+    paramanVue([...TestData.categories , ...TestData.categorized_parameters])
+
+    next(() => {
+
+      expect(window.location.hash)
+      .toBe(`#${activeCategory.id}`)
+
       done()
     }, 10)
   })
